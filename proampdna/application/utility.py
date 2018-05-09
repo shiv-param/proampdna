b25bdbae9b827fe5489d493076b90594c4f1d648
Shiv Prasad <shiv@param.ai>
2018-05-09 23:05:27 +0530

    Added scrapper for species

---
+++ proampdna/application/utility.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from django.conf import settings
+
+
+def build_url_for_scrap(specie_id):
+    return settings.SCRAP_URL.format(specie_id=specie_id)
+
+
+def scrap(specie_id):
+
+    ret = {}
+    page = requests.get(build_url_for_scrap(specie_id))
+
+    if page.status_code != 200:
+        ret['found'] = False
+        ret['species_data'] = {}
+
+    else:
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+        speciesHead = soup.find_all('strong')[0].get_text()
+
+        if speciesHead.find('Not found:') != -1:
+            ret['found'] = False
+            ret['species_data'] = {}
+        else:
+            speciesDataContent = soup.find_all('pre')[0].get_text()
+            speciesDataContent = re.sub(r'\(([^)]+)\)', ";", speciesDataContent)
+            speciesDataContent = speciesDataContent.split(';')
+            speciesMainData = []
+            for eachSet in speciesDataContent:
+                if len(eachSet.strip()) != 0:
+                    speciesData = eachSet.strip().replace(' ', ';').replace(';;', ';').replace(';;;', ';').replace(
+                        ';;;;', ';').split(';')
+                    speciesMainData.append({
+                        'triplet': speciesData[0],
+                        'amino_acid': speciesData[1],
+                        'fraction': float(speciesData[2]),
+                        'frequency': float(speciesData[3])
+                    })
+            ret['found'] = True
+            ret['species_data'] = {
+                'head': speciesHead,
+                'main': speciesMainData
+            }
+
+    return ret
