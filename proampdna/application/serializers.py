3c4a174f7e7cb0d8cf8eb6a13bc58a81990258f7
Shiv Prasad <shiv@param.ai>
2018-05-10 00:14:34 +0530

    Added serializer for resultset

--- proampdna/application/serializers.py
+++ proampdna/application/serializers.py
@@ -21,3 +21,26 @@ class SpecieSerializer(serializers.ModelSerializer):
     class Meta:
         model = app_models.Specie
         fields = ("specie_id", "specie_head", "specie_data")
+
+
+class ResultSet(object):
+    def __init__(self, **kwargs):
+        for field in ('GCPerc', 'forwardPrimer', 'meltingPoint', 'length', 'frequency', 'reversePrimer'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+class ResultSetSerializer(serializers.Serializer):
+    GCPerc = serializers.IntegerField()
+    forwardPrimer = serializers.CharField(max_length=256)
+    meltingPoint = serializers.FloatField()
+    length = serializers.IntegerField()
+    frequency = serializers.FloatField()
+    reversePrimer = serializers.CharField(max_length=256)
+
+    def create(self, validated_data):
+        return ResultSet(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
