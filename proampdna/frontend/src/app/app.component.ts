845c5507440af8cd784cf0cd048cfa37143ff599
Shiv Prasad <shiv@param.ai>
2018-05-10 23:32:44 +0530

    Added pagination

--- proampdna/frontend/src/app/app.component.ts
+++ proampdna/frontend/src/app/app.component.ts
@@ -19,6 +19,10 @@ export class AppComponent {
 
   private specie_data = {};
   private result_data = {};
+  private total_results = {};
+  private page_offsets = [];
+  private previous = "";
+  private next = "";
 
   private specie_data_loaded: boolean = false;
 
@@ -34,13 +38,84 @@ export class AppComponent {
       }
   }
 
-  public runApp(){
+  public runApp(url){
 
-      let url = this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id;
+      if(!Boolean(url)){
+        url = this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id;
+      }
 
       this.http.get(url).subscribe((res)=>{
           this.result_data = res.results;
-        });
+          this.page_offsets = [];
+          if(Number(res.count) > 100){
+              this.total_results = Number(res.count);
+              this.previous = res.previous;
+              this.next = res.next;
+              let first_page: boolean = true;
+              let last_page: boolean = true;
+              let limit = 100;
+              let offset = 0;
+              if(Boolean(this.previous)){
+                first_page = false;
+                let params = this.previous.split('?')[1].split('&');
+                for(let i=0; i<params.length; i++){
+                  let key=params[i].split('=')[0];
+                  let value=params[i].split('=')[1];
+                  if(key == 'offset') offset = Number(value);
+                }
+                offset += limit;
+              }
+              if(Boolean(this.next)){
+                last_page = false;
+                if(!Boolean(this.previous)){
+                  let params = this.next.split('?')[1].split('&');
+                  for(let i=0; i<params.length; i++){
+                    let key=params[i].split('=')[0];
+                    let value=params[i].split('=')[1];
+                    if(key == 'offset') offset = Number(value);
+                  }
+                  offset -= limit;
+                }
+              }
+              if(!first_page){
+                if(Number((offset/limit)) > 1){
+                  this.page_offsets.push({
+                      url: this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id + "&limit=" + limit + "&offset=0",
+                      page_number: 1,
+                      selected: false,
+                  });
+                }
+                this.page_offsets.push({
+                    url: this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id + "&limit=" + limit + "&offset=" + (offset-limit),
+                    page_number: Number(offset/limit),
+                    selected: false,
+                });
+              }
+              this.page_offsets.push({
+                  url: this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id + "&limit=" + limit + "&offset=" + (offset),
+                  page_number: Number((offset/limit)+1),
+                  selected: true,
+              });
+              if(!last_page){
+                this.page_offsets.push({
+                    url: this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id + "&limit=" + limit + "&offset=" + (offset+limit),
+                    page_number: Number((offset/limit)+2),
+                    selected: false
+                });
+                let max_page = Number(this.total_results/limit);
+                if(max_page.toFixed()*limit < this.total_results){
+                  max_page = max_page.toFixed() + 1;
+                }
+                if(Number((offset/limit)) < max_page){
+                  this.page_offsets.push({
+                      url: this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id + "&limit=" + limit + "&offset=" + ((max_page.toFixed()*limit)-limit),
+                      page_number: max_page.toFixed(),
+                      selected: false,
+                  });
+                }
+              }
+          }
+      });
 
   }
 
