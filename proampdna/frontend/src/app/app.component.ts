8fd657c2cc9a4fbcc0a0ccc1a38595050f376b12
Shiv Prasad <shiv@param.ai>
2018-05-10 14:03:39 +0530

    Added Fetch specied_data method

--- proampdna/frontend/src/app/app.component.ts
+++ proampdna/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-// import { Http, Response } from '@angular/http';
-// import { Observable } from 'rxjs';
-// import 'rxjs/add/operator/toPromise';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/toPromise';
 
 
 @Component({
@@ -19,8 +19,24 @@ export class AppComponent {
   private primer_length: string = "12";
   private email: string = "shiv.prsd19@gmail.com";
 
-  // constructor(private http : Http){}
+  private specie_data = {
+      id: '',
+      head: '',
+      data: []
+  };
 
+  // constructor(private http: Http){}
+
+  public fetchSpeciesData(){
+      if(this.specie_id.trim() != ""){
+          let url = this.base_url + "/api/load-species-data/?specie_id=" + this.specie_id;
+          this.http.get(url).toPromise().then((res)=>{
+            console.log(res);
+          }).catch((err)=>{
+            console.log(err)
+          });
+      }
+  }
   // public runApp(){
   //
   //     let url = this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id;
