d420ff9115270dacddf29306c6fa72ea49b39909
Shiv Prasad <shiv@param.ai>
2018-05-10 12:59:43 +0530

    Integrated models in UI

--- proampdna/frontend/src/app/app.component.ts
+++ proampdna/frontend/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+// import { Http, Response } from '@angular/http';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/toPromise';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  private title = 'app';
+  private base_url: string = "http://localhost:8000";
+
+  private specie_id: string = "9606";
+  private amino_acid_sequence: string = "ATCDQYWEGLFHYKIRPHVVVPYQM";
+  private primer_length: string = "12";
+  private email: string = "shiv.prsd19@gmail.com";
+
+  // constructor(private http : Http){}
+
+  // public runApp(){
+  //
+  //     let url = this.base_url + "/api/run-app/?amino_acid_seq=" + this.amino_acid_sequence + "&email=" + this.email + "&primer_len=" + this.primer_length + "&specie_id="+this.specie_id;
+  //
+  //     this.http.get(url)
+  //       .toPromise()
+  //       .then((res)=>{
+  //         console.log(res);
+  //       }).catch((err) => {
+  //         console.log(err)
+  //       });
+  //
+  // }
 }
--- proampdna/frontend/src/app/app.module.ts
+++ proampdna/frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     NgbModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
